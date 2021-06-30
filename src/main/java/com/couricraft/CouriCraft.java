42e91dfa4e00a361274ffc034ff0cc62a5de926f
Jake <jakesterizer@gmail.com>
2021-06-29 20:09:23 -0400

    Progress

--- src/main/java/com/couricraft/CouriCraft.java
+++ src/main/java/com/couricraft/CouriCraft.java
@@ -2,12 +2,14 @@ package com.couricraft;
 
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.ProtocolManager;
+import dev.thejakwe.tuinity.event.AnvilRenameEvent;
 import dev.thejakwe.tuinity.event.MsgCommandEvent;
 import io.papermc.paper.event.player.AsyncChatEvent;
 import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.GenericEvent;
+import net.dv8tion.jda.api.events.guild.member.GuildMemberRemoveEvent;
 import net.dv8tion.jda.api.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.EventListener;
 import net.dv8tion.jda.api.requests.GatewayIntent;
@@ -15,7 +17,9 @@ import net.kyori.adventure.text.Component;
 import net.kyori.adventure.text.TextComponent;
 import net.kyori.adventure.text.format.NamedTextColor;
 import net.kyori.adventure.text.serializer.legacy.LegacyComponentSerializer;
+import org.bukkit.OfflinePlayer;
 import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -26,9 +30,12 @@ import org.bukkit.event.server.ServerListPingEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.awt.*;
+import java.io.File;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.time.Instant;
 import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.ThreadLocalRandom;
 import java.util.logging.Level;
@@ -37,6 +44,7 @@ public final class CouriCraft extends JavaPlugin implements Listener, EventListe
 
     public static CouriCraft instance;
     public FileConfiguration config;
+    public YamlConfiguration whitelist;
     public ProtocolManager protocolManager;
     public JDA jda;
 
@@ -45,14 +53,14 @@ public final class CouriCraft extends JavaPlugin implements Listener, EventListe
         instance = this;
         config = getConfig();
         protocolManager = ProtocolLibrary.getProtocolManager();
+        whitelist = YamlConfiguration.loadConfiguration(new File(getDataFolder(), "whitelist.yml"));
 
         getServer().getPluginManager().registerEvents(this, this);
 
         try {
-            jda = JDABuilder.createLight(config.getString("token"), GatewayIntent.GUILD_MESSAGES)
-                .addEventListeners(this).build().awaitReady();
+            jda = JDABuilder.createLight(config.getString("token"), GatewayIntent.GUILD_MESSAGES).addEventListeners(this).build().awaitReady();
         } catch (Exception e) {
-            getLogger().log(Level.SEVERE, "jda build exception", e);
+            getLogger().log(Level.SEVERE, "JDA build exception", e);
         }
         getLogger().info("CouriCraft Plugin Enabled!");
     }
@@ -106,52 +114,89 @@ public final class CouriCraft extends JavaPlugin implements Listener, EventListe
         ).complete();
     }
 
+    @EventHandler
+    public void itemRename(AnvilRenameEvent event) {
+        event.name(event.name()
+            .replaceAll("[^ -~]", "") // all non ascii chars
+            .replaceAll(config.getString("automod"), "#") // no poopy words madge | regex not public dont bypass it FUNgineer
+        ); //
+    }
+
     @Override
     public void onEvent(GenericEvent event) {
         try {
             this.getClass().getMethod("handleEvent", event.getClass()).invoke(this, event);
-        } catch (NoSuchMethodException | IllegalAccessException | InvocationTargetException ex) {}
+        } catch (NoSuchMethodException ex) {
+            // ignore
+        } catch (IllegalAccessException ex) {
+            getLogger().log(Level.SEVERE, "IllegalAccessException invoking JDA event", ex);
+        } catch (InvocationTargetException ex) { // exception within event handling
+            throw new RuntimeException(ex); // move up stack, JDA will handle
+        }
     }
 
-    public void handleeEvent(GuildMessageReceivedEvent event) {
+    public void handleEvent(GuildMessageReceivedEvent event) throws IOException { // ioex cuz i dont care lmao
         if (event.isWebhookMessage() || event.getAuthor().isSystem() || event.getAuthor().isBot()) return;
         if (event.getChannel().getId().contentEquals(config.getString("channels.whitelist"))) {
-            Player player = getServer().getPlayerExact(event.getMessage().getContentRaw());
-            if (player == null) {
+            UUID uuid = getServer().getPlayerUniqueId(event.getMessage().getContentRaw());
+            if (uuid == null) {
                 event.getChannel().sendMessageEmbeds(
                     new EmbedBuilder()
                         .setTitle("Error")
                         .setColor(Color.RED)
                         .setFooter("CouriCraft")
                         .setTimestamp(Instant.now())
-                        .setDescription("Player `" + event.getMessage().getContentRaw() + "` was not found.")
+                        .setDescription("Could not find player `" + event.getMessage().getContentRaw() + "`")
                         .build()
-                ).complete();
-                return;
-            }
-            if (player.isWhitelisted()) {
-                event.getChannel().sendMessageEmbeds(
-                    new EmbedBuilder()
-                        .setTitle("Error")
-                        .setColor(Color.RED)
-                        .setFooter("CouriCraft")
-                        .setTimestamp(Instant.now())
-                        .setDescription("You are already whitelisted!")
-                        .build()
-                ).complete();
+                ).reference(event.getMessage()).queue();
                 return;
             }
 
+            UUID old = Optional.ofNullable(whitelist.getString(event.getAuthor().getId())).map(UUID::fromString).orElse(null);
+            whitelist.set(event.getAuthor().getId(), uuid.toString());
+            whitelist.save(new File(getDataFolder(), "whitelist.yml"));
+            OfflinePlayer player = getServer().getOfflinePlayer(uuid);
+            if (!whitelist.getValues(false).containsValue(old)) {
+                OfflinePlayer p = getServer().getOfflinePlayer(old);
+                getLogger().info("Unwhitelisted acc %s (%s) | user %s switched to %s (%s)".formatted(p.getName(), old, event.getAuthor().getId(), player.getName(), uuid));
+                p.setWhitelisted(false);
+            } else {
+                getLogger().info("Whitelisted acc %s (%s) via user %s".formatted(player.getName(), uuid, event.getAuthor().getId()));
+            }
+            player.setWhitelisted(true);
             event.getChannel().sendMessageEmbeds(
                 new EmbedBuilder()
                     .setTitle("Success")
                     .setColor(Color.GREEN)
                     .setFooter("CouriCraft")
                     .setTimestamp(Instant.now())
-                    .setDescription("Your account `" + player.getName() + "` was whitelisted successfully\nUUID: `" + player.getUniqueId() + "`")
-                    .setThumbnail("https://crafatar.com/renders/head/" + player.getUniqueId())
+                    .setDescription("Your account `%s` was whitelisted successfully\nUUID: `%s`".formatted(player.getName(), uuid))
+                    .setThumbnail("https://crafatar.com/renders/head/%s.png?overlay=true".formatted(uuid))
                     .build()
-            ).complete();
+            ).reference(event.getMessage()).queue();
         }
+    }
+
+    public void handleEvent(GuildMemberRemoveEvent event) {
+        UUID uuid = Optional.ofNullable(whitelist.getString(event.getUser().getId())).map(UUID::fromString).orElse(null);
+        if (uuid == null) return;
+        whitelist.set(event.getUser().getId(), null);
+        OfflinePlayer player = getServer().getOfflinePlayer(uuid);
+        if (!whitelist.getValues(false).containsValue(uuid)) {
+            player.setWhitelisted(false);
+            getLogger().info("User %s left | unwhitelisted acc %s (%s)".formatted(event.getUser().getId(), player.getName(), uuid));
+        } else {
+            getLogger().info("User %s left | didnt unwhitelist acc %s (%s)".formatted(event.getUser(), player.getName(), uuid));
+        }
+        jda.getTextChannelById(config.getString("channels.logs")).sendMessageEmbeds(
+            new EmbedBuilder()
+                .setTitle("User Left")
+                .setColor(Color.RED)
+                .setAuthor(event.getUser().getAsTag(), event.getUser().getEffectiveAvatarUrl())
+                .setFooter("CouriCraft")
+                .setTimestamp(Instant.now())
+                .setDescription("User %s left, their account `%s` was unwhitelisted.\nUUID: `%s`".formatted(event.getUser().getAsMention(), player.getName(), uuid))
+                .build()
+        ).queue();
     }
 }
